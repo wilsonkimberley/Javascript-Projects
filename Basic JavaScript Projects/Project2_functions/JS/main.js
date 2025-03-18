5f71433a62ef680592cf90e2a936458437e9eefd
wilsonkimberley <kimberleyannuk@gmail.com>
2025-03-18 20:41:10 +0000

    Project 2 Functions Assignment

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+//a function that concatenates two strings and uses the += operator
+
+function My_Function() {
+    var sentence = "I am learning"; //variable sentence
+    sentence += " a lot from this book!"; //+= operator
+    document.getElementById("Concatenate").innerHTML = sentence; //display the concatenated string
+}
