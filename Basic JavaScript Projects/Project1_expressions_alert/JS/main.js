0f01c91d3ae34e658f772146cc0f060eac48d47e
wilsonkimberley <kimberleyannuk@gmail.com>
2025-03-18 20:13:51 +0000

    Basic Java assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+//main.js
+
+//window.alert method
+window.alert("Hello, World!");
+
+//document.write method
+document.write("For the last time, hello, world!");
+
+//creating two variables
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string.";
+
+//displaying the variables using the document.write method
+document.write(Sent1 + Sent2);
+
+//an expression
+document.write(33 * 84);
